41799fbad13bebd133d39295e54aa08fc12faa6b
vimtor <vn4varro@gmail.com>
2024-11-18 09:39:53 +0100

    feat: add support for Redis cluster

--- src/drivers/redis.ts
+++ src/drivers/redis.ts
@@ -1,12 +1,14 @@
-import Redis, { RedisOptions } from "ioredis";
+import { Redis, Cluster, RedisOptions } from "ioredis";
 import { LockDriver } from "../index";
 
 export class RedisDriver implements LockDriver {
-  private redis: Redis;
+  private redis: Redis | Cluster;
 
-  constructor(connection: RedisOptions | Redis) {
+  constructor(connection: RedisOptions | Redis | Cluster) {
     this.redis =
-      connection instanceof Redis ? connection : new Redis(connection);
+      connection instanceof Redis || connection instanceof Cluster
+        ? connection
+        : new Redis(connection);
   }
 
   public async lock(key: string, expires: number) {
